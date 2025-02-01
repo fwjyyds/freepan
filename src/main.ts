4087faac1caa5b913003a2a54ae23467410d0e7c
YUFENG <3498167480@qq.com>
2025-02-01 19:05:39 +0800

    r1:login1_25_2_1_19

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,24 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import {
+    // create naive ui
+    create,
+    // component
+    NButton,
+    NModal
+  } from 'naive-ui'
+
+  
+  const naive = create({
+    components: [NButton,NModal]
+  })
+  
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+  app.use(naive)
 app.mount('#app')
